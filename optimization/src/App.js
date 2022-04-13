b1835d42845472bd2053988393a9ee4845d5de48
abrar4799 <abrar.it99@gmail.com>
2022-04-13 04:04:12 +0200

    add toggle button

--- optimization/src/App.js
+++ optimization/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from "./components/UI/Button/Button"
 
 import './App.css';
 
 function App() {
+  const [showPara , setShowPare] = useState(false)
+
+  const ToggleParaHandler = () =>{
+    setShowPare( (prevShowPara) => !showPara )
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
+     { showPara && <p>This is new!</p>}
+     <Button onClick={ToggleParaHandler}>Toggle Para!</Button>
+    
     </div>
   );
 }
